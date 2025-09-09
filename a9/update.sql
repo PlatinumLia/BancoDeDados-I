b0697d6144b0d9724bdc4fdf28bd6c14d1da7d95
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2025-09-08 22:09:27 -0300

    Add files via upload

---
+++ a9/update.sql
@@ -0,0 +1,5 @@
+-- Altera o nome do professor de id 9
+UPDATE professor SET nome = 'Isabela Marins' WHERE id_professor = 9;
+
+-- Excluir o professor de id 9
+DELETE FROM professor WHERE id_professor = 9;
