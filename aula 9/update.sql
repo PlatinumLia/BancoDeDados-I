8e37c54be849c02f22e8f5bfe8e74acc929cafb8
PlatinumLia <134401063+PlatinumLia@users.noreply.github.com>
2025-09-08 22:09:08 -0300

    Delete aula 9 directory

--- aula 9/update.sql
+++
@@ -1,5 +0,0 @@
--- Altera o nome do professor de id 9
-UPDATE professor SET nome = 'Isabela Marins' WHERE id_professor = 9;
-
--- Excluir o professor de id 9
-DELETE FROM professor WHERE id_professor = 9;
